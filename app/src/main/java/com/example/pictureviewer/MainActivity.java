c568fb102e981ce86f04dcf66b376b09364966ee
Betzabeth Narvaez <betzynarv@gmail.com>
2021-10-10 20:21:00 +0900

    added images

--- app/src/main/java/com/example/pictureviewer/MainActivity.java
+++ app/src/main/java/com/example/pictureviewer/MainActivity.java
@@ -14,5 +14,9 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+
+        binding.catBtn.setOnClickListener(v -> binding.animalView.setImageResource(R.drawable.cat));
+        binding.dogBtn.setOnClickListener(v -> binding.animalView.setImageResource(R.drawable.dog));
+        binding.hamsterBtn.setOnClickListener(v -> binding.animalView.setImageResource(R.drawable.hamster));
     }
 }
